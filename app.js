6918abf0e3291494cdc32c11efe5fdb7d29d7ef2
SwagMessiah02 <vmdemesquita@gmail.com>
2026-01-25 16:20:01 -0300

    Configurando ambiente

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes.js');
 const path = require('path');
+const cookieParser = require('cookie-parser')
+require("dotenv/config")
 
 const app = express();
 const PORT = 3000;
@@ -8,6 +10,7 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
 
 app.use('/', routes);
 
